59475597b4e8f4840f6aa549fb617fc65dce32d7
Ahmed Amin <qa.ahmed.amin10@gmail.com>
2021-05-28 23:43:40 +0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,3 @@
-// const webpack = require('webpack');
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './src/index.js',
-
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     publicPath: '/dist/',
-//     filename: 'main.js',
-//   },
-
-//   module: {
-//     rules: [
-//       {
-//         test: [/\.vert$/, /\.frag$/],
-//         use: 'raw-loader',
-//       },
-//     ],
-//   },
-
-//   plugins: [
-//     new webpack.DefinePlugin({
-//       CANVAS_RENDERER: JSON.stringify(true),
-//       WEBGL_RENDERER: JSON.stringify(true),
-//     }),
-//   ],
-
-// };
-
 const path = require('path');
 
 module.exports = {
